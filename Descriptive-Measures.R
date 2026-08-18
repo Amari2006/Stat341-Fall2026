ca4c8175d6c9f14cf16127178b8d511212784cdb
Amari Way <amway@claflin.edu>
2026-08-18 16:15:15 -0400

    Add files via upload

---
+++ Descriptive-Measures.R
@@ -0,0 +1,26 @@
+#Amari Way, 8/18/26, Test Functions of Descriptive Statistics
+
+#Create a variable called variable1 and store 5 numbers from 1-5
+
+variable1 <- c(1,2,3,4,5)
+
+#Calculate mean of variable1
+mean(variable1)
+[1] 3
+
+#Caluculate standard deviation of variable1
+sd(variable1)
+[1] 1.581139
+
+#Caluculate median for variable1
+median(variable1)
+[1] 3
+
+#Calculate mode for variable1
+get_mode(variable1)
+ 5
+
+
+
+
+
